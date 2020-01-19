cf54ce7803a57543892065b19d454c708458a3d7
Felipe Mantovani <felipexmantovani@gmail.com>
2020-01-19 17:07:41 -0300

    Implementado socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('../src/routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-q4y1o.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -13,4 +18,5 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-q4y1o.mongodb.net/w
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+
+server.listen(3333);
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,11 +5,12 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null);
-  const [techs, setTechs] = useState([]);
+  const [techs, setTechs] = useState('');
 
   useEffect(() => {
     async function loadInitialPosition() {
@@ -31,6 +32,18 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
+
+  function setupWebsocket() {
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+
+    connect(latitude, longitude, techs);
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -38,11 +51,12 @@ function Main({ navigation }) {
       params: {
         latitude,
         longitude,
-        techs: techs
+        techs
       }
     });
 
     setDevs(response.data.devs);
+    setupWebsocket();
   }
 
   function handleRegionChange(region) {
